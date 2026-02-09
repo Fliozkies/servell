a25374e307f0fcfc150242ac93743d2fc1fef162
Shuani <obenzaj5@gmail.com>
2026-02-09 22:12:33 +0800

    Initial notification

---
+++ app/juarez_app/pages/notification.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function NotificationScreen() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Nothing here, yet</Text>
+    </View>
+  );
+}
